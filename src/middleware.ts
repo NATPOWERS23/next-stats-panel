f0271e3659b8f24a57f0e1746defd52c05fcf25c
Natalie <tiredjosefine90210@gmail.com>
2024-07-05 17:08:13 +0300

    [feat] added middleware with protected paths

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,77 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([
+  '/((?!api|_next/static|_next/image|favicon.ico|дщпшт|verifyemail|denied).*)',
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
+// export function middleware(request: NextRequest) {
+//   try {
+//     const path = request.nextUrl.pathname;
+
+//     const adminBlockedPaths = [''];
+//     const chanelOwnerBlockedPaths = ['/websites/landing', '/events'];
+//     const managerPaths = ['/events', '/profile', '/releases', '/team', '/tasks', '/logout'];
+//     const clientPaths = ['/events', '/logout'];
+
+//     // Define paths that are considered public (accessible without a token)
+//     const isPublicPath = path === '/login' || path === '/signup' || path === '/verifyemail' || path === '/denied';
+
+//     const isClientUserPath = clientPaths.includes(path);
+//     const isManagerPath = managerPaths.includes(path);
+//     const isAdminPath = !adminBlockedPaths.includes(path);
+//     const isChanelOwnerPath = !chanelOwnerBlockedPaths.includes(path);
+
+//     const token = request.cookies.get('token')?.value || '';
+//     const roleCookies = request.cookies.get('role')?.value;
+//     const role = roleCookies ? +JSON.parse(roleCookies) : ROLE.CLIENT;
+
+//     // Redirect logic based on the path, user role and token presence
+//     if (path === '/logout' && !token) {
+//       return NextResponse.redirect(new URL('/login', request.nextUrl));
+//     }
+
+//     if (isPublicPath && token) {
+//       return NextResponse.redirect(new URL('/', request.nextUrl));
+//     }
+
+//     if (!isPublicPath && !token) {
+//       return NextResponse.redirect(new URL('/login', request.nextUrl));
+//     }
+
+//     if (!isPublicPath && token) {
+//       if (!isAdminPath && role === ROLE.ADMIN) {
+//         return NextResponse.redirect(new URL('/', request.nextUrl));
+//       }
+
+//       if (!isChanelOwnerPath && role === ROLE.CHANNEL_OWNER) {
+//         return NextResponse.redirect(new URL('/profile', request.nextUrl));
+//       }
+
+//       if (!isClientUserPath && role === ROLE.CLIENT) {
+//         return NextResponse.redirect(new URL('/events', request.nextUrl));
+//       }
+
+//       if (!isManagerPath && role === ROLE.MANAGER) {
+//         return NextResponse.redirect(new URL('/denied', request.nextUrl));
+//       }
+//     }
+//   } catch (error) {
+//     console.log('error: ', error);
+//   }
+// }
+
+// // It specifies the paths for which this middleware should be executed.
+// export const config = {
+//   matcher: [
+//     // Match all routes except the ones that start with public paths and api and the static folder.
+//     '/((?!api|_next/static|_next/image|favicon.ico|login|signup|verifyemail|denied).*)',
+//   ],
+// };
