6b8aa9c12571a221af27be02229013aead0c0939
Natalie <tiredjosefine90210@gmail.com>
2024-08-20 09:07:27 +0300

    [fix] fixed build issues

--- src/app/crm/videos-statistic/page.tsx
+++ src/app/crm/videos-statistic/page.tsx
@@ -1,19 +1,19 @@
 import InfoBox from '@/components/InfoBox/InfoBox';
 
-export const statCards = [
-  { id: '1', title: 'Stream Duration', numbers: 120001, diff: 100 },
-  { id: '2', title: 'Avarage Viewers', numbers: 120001, diff: 100 },
-  { id: '3', title: 'Max Viewers', numbers: 120001, diff: 100 },
-  { id: '4', title: 'Unique Viewers', numbers: 120001, diff: 100 },
-  { id: '5', title: 'Unique Chatters', numbers: 120001, diff: 100 },
-  { id: '6', title: 'Live Views', numbers: 120001, diff: 100 },
-  { id: '7', title: 'Followers', numbers: 120001, diff: 100 },
-  { id: '8', title: 'New subscribers', numbers: 120001, diff: 100 },
-];
-
-const description = ' from last stream';
-
 export default function VideosStatistic() {
+  const statCards = [
+    { id: '1', title: 'Stream Duration', numbers: 120001, diff: 100 },
+    { id: '2', title: 'Avarage Viewers', numbers: 120001, diff: 100 },
+    { id: '3', title: 'Max Viewers', numbers: 120001, diff: 100 },
+    { id: '4', title: 'Unique Viewers', numbers: 120001, diff: 100 },
+    { id: '5', title: 'Unique Chatters', numbers: 120001, diff: 100 },
+    { id: '6', title: 'Live Views', numbers: 120001, diff: 100 },
+    { id: '7', title: 'Followers', numbers: 120001, diff: 100 },
+    { id: '8', title: 'New subscribers', numbers: 120001, diff: 100 },
+  ];
+  
+  const description = ' from last stream';
+
   return (
     <div className="page-wrapper">
       <h1>Videos Statistic</h1>
