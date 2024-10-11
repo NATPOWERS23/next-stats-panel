db7c19b99aeec011357910f72fb4b845bacd1dae
Natalie <tiredjosefine90210@gmail.com>
2024-10-11 21:31:58 +0200

    [fix] navbar UI

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,53 +2,56 @@ import { Protect, UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
 
-import styles from "./Navbar.module.css";
 import { ROLE, roleList } from "@/constants/roles";
-import Icon from "../Icon/Icon";
 import TwitchButton from "../TwitchButton/TwitchButton";
+import styles from "./Navbar.module.css";
 
 export default function Navbar() {
 	const { userId } = auth();
 
-	const openTwitchFormModal = () => {};
+	const list = (
+		<ul className={styles.navbarList}>
+			{!userId ? (
+				<>
+					<div className={styles.item}>
+						<Link href="/sign-in">
+							<li>Login</li>
+						</Link>
+					</div>
+
+					<div className={styles.item}>
+						<Link href="/sign-up">
+							<li>Sign Up</li>
+						</Link>
+					</div>
+				</>
+			) : (
+				<>
+					<Protect role={roleList[ROLE.channel_owner]}>
+						<div className={styles.item}>
+							<TwitchButton />
+						</div>
+					</Protect>
+
+					<div className={styles.item}>
+						<Link href="/profile">
+							<li>Profile</li>
+						</Link>
+					</div>
+
+					<li>
+						<UserButton />
+					</li>
+				</>
+			)}
+		</ul>
+	);
 
 	return (
-		<nav className={styles.navbar}>
-			<ul className={styles.navbarList}>
-				{!userId ? (
-					<>
-						<div className={styles.item}>
-							<Link href="/sign-in">
-								<li>Login</li>
-							</Link>
-						</div>
-
-						<div className={styles.item}>
-							<Link href="/sign-up">
-								<li>Sign Up</li>
-							</Link>
-						</div>
-					</>
-				) : (
-					<>
-						<Protect role={roleList[ROLE.channel_owner]}>
-							<div className={styles.item}>
-								<TwitchButton />
-							</div>
-						</Protect>
-
-						<div className={styles.item}>
-							<Link href="/profile">
-								<li>Profile</li>
-							</Link>
-						</div>
-
-						<li>
-							<UserButton />
-						</li>
-					</>
-				)}
-			</ul>
-		</nav>
+		<>
+			<nav className="right-0 hidden sm:flex fixed z-10">{list}</nav>
+			{/* mobile navbar */}
+			<nav className="sm:hidden left-0 bottom-0 w-full fixed z-10">{list}</nav>
+		</>
 	);
 }
