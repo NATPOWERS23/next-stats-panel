086bc041628c82751c5f49dd7299f4b60499486e
Natalie <tiredjosefine90210@gmail.com>
2024-11-14 11:45:58 +0100

    [UI] fix Manage ratins style

--- src/features/crm/ManagerRating/ManagerRating.tsx
+++ src/features/crm/ManagerRating/ManagerRating.tsx
@@ -26,46 +26,64 @@ export default function ManagerRating({
 	};
 
 	return (
-		<div className="card">
-			<div className="card-body">
-				<div className="header flex">
+		<div className="card w-full px-6 pt-2 pb-4 rounded-2large shadow-lg bg-green flex flex-col items-center">
+			<div className="card-body w-full">
+				<div className="header flex w-full justify-end">
+					<div className="header-title py-4 font-black text-sm sm:text-xl w-full">
+						Manager Rating
+					</div>
 					<div className="header-actions">
-						<Button content={<Icon name="share" />} onClick={openShareModal} />
+						<Button
+							content={<Icon name="share" />}
+							onClick={openShareModal}
+							size="small"
+							buttonType="outlined"
+						/>
 					</div>
 				</div>
 				<div className="main">
-					<div className="main-text">Manager Rating</div>
-					<ul>
-						<div>
-							<span>Deals</span>
+					<div className="grid grid-cols-[repeat(5_minmax(0,_auto))] items-center">
+						<>
+							<span className="col-start-3">Deals</span>
 							<span>Profit</span>
-						</div>
+							<span> </span>
+						</>
 						{data.rating.length &&
 							data.rating.map((item, index) => (
 								<>
-									<li key={item.name}>
-										{item.avatarSrc ? (
-											<Avatar sizes={24} src={item.avatarSrc} />
-										) : (
-											<Avatar sizes={24} />
-										)}
-										<span>{item.name}</span>
-										<Button
-											content={item.dealsAmount.toString()}
-											onClick={undefined}
-										/>
-										<Button content={`$ ${item.profit}`} onClick={undefined} />
-										<span>#{index + 1}</span>
-									</li>
-									<hr key={item.name} />
+									{item.avatarSrc ? (
+										<Avatar key={item.name} sizes={24} src={item.avatarSrc} />
+									) : (
+										<Avatar key={item.name} sizes={24} />
+									)}
+									<span key={item.name}>{item.name}</span>
+									<Button
+										key={item.name}
+										content={item.dealsAmount.toString()}
+										onClick={undefined}
+										size="small"
+									/>
+									<Button
+										key={item.name}
+										content={`$${item.profit}`}
+										onClick={undefined}
+										fullWidth={true}
+										size="small"
+									/>
+									<span key={item.name} className="flex justify-end">
+										#{index + 1}
+									</span>
+									<hr key={item.name} className="col-start-1 col-end-6 my-2" />
 								</>
 							))}
-					</ul>
+					</div>
 				</div>
 				<div className="footer">
-					<div className="flex">
-						<div className="footer-numbers">${data.totalAmount}</div>
-						<div className="footer-subtext">
+					<div className="flex gap-2 justify-start">
+						<div className="footer-numbers font-black text-xs sm:text-xl">
+							${data.totalAmount}
+						</div>
+						<div className="footer-subtext font-thin text-xs w-40 ">
 							Avarage revenue per trade ${data.avarageRevenue}
 						</div>
 					</div>
