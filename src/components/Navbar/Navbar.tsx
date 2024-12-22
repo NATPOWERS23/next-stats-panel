ea8a4f0e8ef906fe22bc78fc52645d979c141e5d
Natalie <tiredjosefine90210@gmail.com>
2024-12-22 12:29:25 +0100

    [feat] added clerk navigation

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,55 +7,73 @@ import TwitchButton from "../TwitchButton/TwitchButton";
 import ClerkLogoutButton from "../ClerkLogoutButton/ClerkLogoutButton";
 
 export default function Navbar() {
-	const { userId, has } = auth();
+  const { userId, has } = auth();
 
-	const userHasRole =
-		has({ role: "org:member" }) ||
-		has({ role: "org:channel_owner" }) ||
-		has({ role: "org:adminr" });
+  const userHasRole =
+    has({ role: "org:member" }) ||
+    has({ role: "org:channel_owner" }) ||
+    has({ role: "org:adminr" });
 
-	return (
-		<nav className={styles.navbar}>
-			<ul className={styles.navbarList}>
-				{!userId ? (
-					<>
-						<div className={styles.item}>
-							<Link href="/sign-in">
-								<li>Login</li>
-							</Link>
-						</div>
+  const adminRole = has({ role: "org:admin" });
 
-						<div className={styles.item}>
-							<Link href="/sign-up">
-								<li>Sign Up</li>
-							</Link>
-						</div>
-					</>
-				) : (
-					<>
-						{!userHasRole && <TwitchButton />}
+  return (
+    <nav className={styles.navbar}>
+      <ul className={styles.navbarList}>
+        {!userId ? (
+          <>
+            <div className={styles.item}>
+              <Link href="/sign-in">
+                <li>Login</li>
+              </Link>
+            </div>
 
-						<li>
-							<UserButton />
-						</li>
+            <div className={styles.item}>
+              <Link href="/sign-up">
+                <li>Sign Up</li>
+              </Link>
+            </div>
+          </>
+        ) : (
+          <>
+            {!userHasRole && <TwitchButton />}
 
-						<div className={styles.item}>
-							<Link href="/profile">
-								<li>Profile</li>
-							</Link>
-						</div>
-					</>
-				)}
-				<div className={styles.item}>
-					<Link href="/client">
-						<li>Client</li>
-					</Link>
-				</div>
+            <li>
+              <UserButton />
+            </li>
 
-				<li>
-					<ClerkLogoutButton />
-				</li>
-			</ul>
-		</nav>
-	);
+            {adminRole &&
+              <>
+                <div className={styles.item}>
+                  <Link href="https://dashboard.clerk.com/" rel="noopener noreferrer" target="_blank">
+                    <li>Clerk</li>
+                  </Link>
+                </div>
+
+                <div className={styles.item}>
+                  <Link href="https://cloud.mongodb.com/v2/" rel="noopener noreferrer" target="_blank">
+                    <li>MongoDB</li>
+                  </Link>
+                </div>
+              </>
+            }
+
+            <div className={styles.item}>
+              <Link href="/profile">
+                <li>Profile</li>
+              </Link>
+            </div>
+          </>
+        )}
+        <div className={styles.item}>
+          <Link href="/client">
+            <li>Client</li>
+          </Link>
+        </div>
+
+        <li>
+          <ClerkLogoutButton />
+        </li>
+      </ul>
+    </nav>
+  );
 }
